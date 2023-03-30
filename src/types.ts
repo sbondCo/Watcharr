6879ff790a53bfd0cfab8755d52a20d50be92c30
IRHM <37304121+IRHM@users.noreply.github.com>
2023-03-30 23:12:35 +0100

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,23 @@
 export interface Content {
-  /**
-   * Title of content.
-   */
-  title: string;
-
-  /**
-   * URL to poster image.
-   */
-  poster: string;
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  title?: string;
+  original_language: string;
+  original_title?: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  genre_ids: number[];
+  popularity: number;
+  release_date?: string;
+  video?: boolean;
+  vote_average: number;
+  vote_count: number;
+  name?: string;
+  original_name?: string;
+  first_air_date?: string;
+  origin_country?: string[];
 }
 
 export interface Watched {
