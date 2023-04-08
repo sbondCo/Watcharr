528b327537175906632ff373e76be37f5decd1d3
IRHM <37304121+IRHM@users.noreply.github.com>
2023-04-08 16:03:39 +0100

    UI: Show status/rating on watched items in search

--- src/routes/search/[query]/+page.svelte
+++ src/routes/search/[query]/+page.svelte
@@ -2,10 +2,14 @@
   import req from "@/lib/api";
   import Poster from "@/lib/Poster.svelte";
   import PosterList from "@/lib/PosterList.svelte";
+  import { watchedList } from "@/store";
   import type { ContentType, Rating, WatchedAddRequest, WatchedStatus } from "@/types";
+  import { get } from "svelte/store";
   import type { ContentSearch } from "./+page";
 
   export let data: ContentSearch;
+
+  let wList = get(watchedList);
 
   function addWatched(
     contentId: number,
@@ -20,6 +24,15 @@
       status
     } as WatchedAddRequest);
   }
+
+  function getWatchedDependedProps(wid: number) {
+    const wel = wList.find((wl) => wl.content.id === wid);
+    if (!wel) return {};
+    return {
+      status: wel.status,
+      rating: wel.rating
+    };
+  }
 </script>
 
 <svelte:head>
@@ -27,12 +40,13 @@
 </svelte:head>
 
 <PosterList>
-  {#each data.results as w}
+  {#each data.results as w (w.id)}
     <Poster
       poster={"https://image.tmdb.org/t/p/w500" + w.poster_path}
       title={w.title ?? w.name}
       desc={w.overview}
       onBtnClicked={(t, r) => addWatched(w.id, w.title ? "movie" : "tv", t, r)}
+      {...getWatchedDependedProps(w.id)}
     />
   {/each}
 </PosterList>
--- src/store.ts
+++ src/store.ts
@@ -2,5 +2,3 @@ import { writable } from "svelte/store";
 import type { Watched } from "./types";
 
 export const watchedList = writable<Watched[]>([]);
-
-watchedList.subscribe((wl) => console.log(wl));
