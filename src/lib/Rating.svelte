7f931ec8ef09f70b3e929caad9c5efe29c6de67f
IRHM <37304121+IRHM@users.noreply.github.com>
2023-04-17 18:42:52 +0100

    🐥 Create Rating and Status components

---
+++ src/lib/Rating.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+  export let rating: number | undefined;
+  let hoveredRating: number | undefined;
+  let shownRating: number | undefined;
+
+  const ratingDesc = [
+    "Apalling",
+    "Horrible",
+    "Very Bad",
+    "Bad",
+    "Average",
+    "Fine",
+    "Good",
+    "Very Good",
+    "Great",
+    "Masterpiece"
+  ];
+
+  function handleStarClick(r: number) {
+    rating = r;
+  }
+
+  $: {
+    if (hoveredRating !== undefined) shownRating = hoveredRating;
+    else if (rating !== undefined) shownRating = rating;
+  }
+
+  function handleStarHover(r: number) {
+    hoveredRating = r;
+  }
+
+  function handleStarHoverEnd() {
+    hoveredRating = undefined;
+  }
+</script>
+
+<div class="rating-container">
+  {rating}
+  <span>
+    {#if hoveredRating !== undefined}
+      {ratingDesc[hoveredRating]}
+    {:else if rating !== undefined}
+      {ratingDesc[rating]}
+    {:else}
+      Select Your Rating
+    {/if}
+  </span>
+  <div class="rating">
+    {#each [9, 8, 7, 6, 5, 4, 3, 2, 1, 0] as v}
+      <button
+        class="plain{shownRating === v ? ' lit' : ''}"
+        on:click={() => handleStarClick(v)}
+        on:mouseenter={() => handleStarHover(v)}
+        on:mouseleave={() => handleStarHoverEnd()}
+      >
+        *
+      </button>
+    {/each}
+  </div>
+</div>
+
+<style lang="scss">
+  .rating-container {
+    display: flex;
+    flex-flow: column;
+  }
+
+  .rating {
+    display: flex;
+    flex-flow: row-reverse;
+    align-items: center;
+    justify-content: center;
+    color: black;
+    -webkit-text-stroke: 1.5px black;
+    cursor: pointer;
+    overflow: hidden;
+    margin: 10px 0 10px 0;
+
+    button {
+      font-size: 55px;
+      font-family: "Rampart One";
+      letter-spacing: 10px;
+      line-height: 52px;
+      height: 38px;
+
+      &:global(.lit),
+      &:global(.lit ~ button) {
+        color: gold;
+        -webkit-text-stroke: 1.5px gold;
+      }
+
+      @media screen and (max-width: 500px) {
+        font-size: 40px;
+      }
+    }
+  }
+</style>
---
+++ src/lib/Status.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import Icon from "./Icon.svelte";
+  import type { WatchedStatus } from "../types";
+
+  export let status: WatchedStatus | undefined;
+  export let onChange: (newStatus: WatchedStatus) => void;
+</script>
+
+<div class="status">
+  <button
+    class={status && status !== "PLANNED" ? "not-active" : ""}
+    on:click={() => onChange("PLANNED")}
+  >
+    <Icon i="calendar" />
+  </button>
+  <button
+    class={status && status !== "WATCHING" ? "not-active" : ""}
+    on:click={() => onChange("WATCHING")}
+  >
+    <Icon i="clock" />
+  </button>
+  <button
+    class={status && status !== "FINISHED" ? "not-active" : ""}
+    on:click={() => onChange("FINISHED")}
+  >
+    <Icon i="check" />
+  </button>
+  <button class={status && status !== "HOLD" ? "not-active" : ""} on:click={() => onChange("HOLD")}>
+    <Icon i="pause" />
+  </button>
+  <button
+    class={status && status !== "DROPPED" ? "not-active" : ""}
+    on:click={() => onChange("DROPPED")}
+  >
+    <Icon i="thumb-down" />
+  </button>
+</div>
+
+<style lang="scss">
+  .status {
+    display: flex;
+    flex-flow: row;
+    gap: 10px;
+    width: 100%;
+    height: 100%;
+
+    button {
+      font-size: 10px;
+
+      &:hover {
+        fill: white;
+      }
+    }
+  }
+</style>
