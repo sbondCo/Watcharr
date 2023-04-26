1e6604833c3aa7c7dd7b708dd30071b4c9629ba2
IRHM <37304121+IRHM@users.noreply.github.com>
2023-04-26 21:01:43 +0100

    Create reusable tooltip action - applied to watched statuses

--- src/lib/Status.svelte
+++ src/lib/Status.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Icon from "./Icon.svelte";
   import type { WatchedStatus } from "../types";
+  import tooltip from "./actions/tooltip";
 
   export let status: WatchedStatus | undefined;
   export let onChange: (newStatus: WatchedStatus) => void;
@@ -10,27 +11,35 @@
   <button
     class={status && status !== "PLANNED" ? "not-active" : ""}
     on:click={() => onChange("PLANNED")}
+    use:tooltip={{ text: "Planned", pos: "top" }}
   >
     <Icon i="calendar" />
   </button>
   <button
     class={status && status !== "WATCHING" ? "not-active" : ""}
     on:click={() => onChange("WATCHING")}
+    use:tooltip={{ text: "Watching", pos: "top" }}
   >
     <Icon i="clock" />
   </button>
   <button
     class={status && status !== "FINISHED" ? "not-active" : ""}
     on:click={() => onChange("FINISHED")}
+    use:tooltip={{ text: "Finished", pos: "top" }}
   >
     <Icon i="check" />
   </button>
-  <button class={status && status !== "HOLD" ? "not-active" : ""} on:click={() => onChange("HOLD")}>
+  <button
+    class={status && status !== "HOLD" ? "not-active" : ""}
+    on:click={() => onChange("HOLD")}
+    use:tooltip={{ text: "On Hold", pos: "top" }}
+  >
     <Icon i="pause" />
   </button>
   <button
     class={status && status !== "DROPPED" ? "not-active" : ""}
     on:click={() => onChange("DROPPED")}
+    use:tooltip={{ text: "Dropped", pos: "top" }}
   >
     <Icon i="thumb-down" />
   </button>
