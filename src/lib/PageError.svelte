e4e0c3dd91b7b6bf817709592a36b8f8781d44c0
IRHM <37304121+IRHM@users.noreply.github.com>
2023-05-07 23:19:39 +0100

    Show extra details in PageError if available
    changed error type to any

--- src/lib/PageError.svelte
+++ src/lib/PageError.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
   export let pretty: string;
-  export let error: Error;
+  export let error: any;
 </script>
 
 <div>
   <div>
     <strong>{pretty}</strong>
-    <p>{error.message}</p>
+    {#if error?.message}
+      <p>{error.message}</p>
+      {#if error.response}
+        <p>{error.response.data.error}</p>
+      {/if}
+    {:else}
+      <p>{JSON.stringify(error)}</p>
+    {/if}
   </div>
 </div>
 
@@ -21,17 +28,14 @@
       justify-content: center;
       flex-flow: column;
       max-width: 500px;
-      margin: 0 20px;
-
-      strong {
-        margin-bottom: 10px;
-      }
+      gap: 15px;
 
       p {
         color: white;
         background-color: rgb(66, 60, 60);
         padding: 10px;
         border-radius: 8px;
+        text-transform: capitalize;
       }
     }
   }
