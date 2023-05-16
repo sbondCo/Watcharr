d5f1684a1ed1c008c941b52787e0a0bf5c4d69f2
IRHM <37304121+IRHM@users.noreply.github.com>
2023-05-16 21:59:43 +0100

    Log version to console

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,10 @@
+<script>
+  console.log(
+    `%cWATCHARR v${__WATCHARR_VERSION__}`,
+    "background: white;color: black;font-size: large;padding: 3px 5px;"
+  );
+</script>
+
 <div id="tooltip" />
 <slot />
 
