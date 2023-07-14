15dfcfd477d2dd0a714d2009423ad55d462d2ef8
IRHM <37304121+IRHM@users.noreply.github.com>
2023-07-14 23:27:29 +0100

    start profile page

---
+++ src/routes/(app)/profile/+page.svelte
@@ -0,0 +1,20 @@
+<div class="content">
+  <div class="inner">
+    <h2>Hey {localStorage.getItem("username")}</h2>
+
+    <div class="stats" />
+  </div>
+</div>
+
+<style lang="scss">
+  .content {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+
+    .inner {
+      min-width: 400px;
+      max-width: 400px;
+    }
+  }
+</style>
