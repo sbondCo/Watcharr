736a06d362f583a18d33a838aaf01e6b29a800e9
IRHM <37304121+IRHM@users.noreply.github.com>
2023-08-15 23:23:18 +0100

    #87 Close VideoEmbedModal with escape key
    Also fixed modal div linting issue

--- src/lib/content/VideoEmbedModal.svelte
+++ src/lib/content/VideoEmbedModal.svelte
@@ -1,9 +1,36 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   export let embed: string;
   export let closed: () => void;
+
+  let modalDiv: HTMLDivElement;
+
+  onMount(() => {
+    // For better experience on keyboard.
+    // If modal shown by btn click, the btn will still be
+    // focused, so clicking `esc` wont trigger until tab
+    // is clicked first... which I count as not intuitive.
+    if (modalDiv) {
+      modalDiv.focus();
+    }
+  });
+
+  function handleKeyDown(e: KeyboardEvent) {
+    if (e.key === "Escape") {
+      closed();
+    }
+  }
 </script>
 
-<div class="modal" on:click={closed} on:keydown={closed}>
+<div
+  bind:this={modalDiv}
+  class="modal"
+  on:click={closed}
+  on:keydown={handleKeyDown}
+  role="button"
+  tabindex="0"
+>
   {#if embed}
     <div class="wrapper">
       <iframe title="Video Embed" src={embed} frameborder="0" width="100%" height="100%" />
