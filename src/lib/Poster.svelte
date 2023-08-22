bf0b93f90f1a0f9e51b8fbfec04e0c3ee3a0e1c3
Kurt Gray <94031594+kurtmgray@users.noreply.github.com>
2023-08-21 18:43:27 -0700

    -dateStr
    Co-authored-by: Mr <37304121+IRHM@users.noreply.github.com>

--- src/lib/Poster.svelte
+++ src/lib/Poster.svelte
@@ -41,7 +41,7 @@
   const poster = `https://image.tmdb.org/t/p/w500${media.poster_path}`;
   const link = media.id ? `/${media.media_type}/${media.id}` : undefined;
   const dateStr = media.release_date || media.first_air_date;
-  const year = dateStr ? new Date(dateStr).getFullYear() : undefined;
+  const year = new Date(media.release_date || media.first_air_date)?.getFullYear();
 
   function handleStarClick(r: number) {
     if (r == rating) return;
