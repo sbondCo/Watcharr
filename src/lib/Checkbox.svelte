f1db1ff2b4ce62989ce1af7ede97dc99101c57bb
IRHM <37304121+IRHM@users.noreply.github.com>
2023-08-26 23:56:48 +0100

    Checkbox: Add small delay to reenable
    So on fast networks it doesnt look ugly

--- src/lib/Checkbox.svelte
+++ src/lib/Checkbox.svelte
@@ -3,13 +3,35 @@
   export let toggled: (on: boolean) => void;
   export let disabled = false;
 
+  let actualDisabled = false;
+
+  $: {
+    // In cases where we disable and enable the checkbox super fast
+    // it ends up looking like a jittery mess. To circumvent this
+    // issue, we add a small delay before undisabling.
+    // (eg. fast internet and net request dis and re enables it quickly)
+    if (disabled) {
+      actualDisabled = true;
+    } else {
+      setTimeout(() => {
+        actualDisabled = disabled;
+      }, 150);
+    }
+  }
+
   function checkboxChange(e: Event) {
     toggled((e.target as HTMLInputElement).checked);
   }
 </script>
 
 <div class="toggle-pill-color">
-  <input bind:checked={value} {disabled} type="checkbox" id="checkbox" on:change={checkboxChange} />
+  <input
+    bind:checked={value}
+    disabled={actualDisabled}
+    type="checkbox"
+    id="checkbox"
+    on:change={checkboxChange}
+  />
   <label for="checkbox"></label>
 </div>
 
