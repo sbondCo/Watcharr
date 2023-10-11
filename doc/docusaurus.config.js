4565af412beb4cc34446a9cb25c478710bd621a9
IRHM <37304121+IRHM@users.noreply.github.com>
2023-10-11 23:06:58 +0100

    docs: add workflows

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -25,6 +25,8 @@ const config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
+  trailingSlash: false,
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
