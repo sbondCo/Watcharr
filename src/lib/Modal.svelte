10285bffd78cc1182b65310062c8bdfcd092610f
IRHM <37304121+IRHM@users.noreply.github.com>
2023-10-24 18:45:15 +0100

    Modal: Scroll when not enough space, show above nav

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -29,6 +29,7 @@
     backdrop-filter: blur(2px) saturate(180%);
     background-color: color-mix(in srgb, black 85%, transparent);
     position: fixed;
+    z-index: 99999999999;
   }
 
   .modal {
@@ -40,7 +41,7 @@
     top: 0;
     left: 0;
     position: fixed;
-    z-index: 9999;
+    z-index: 99999999999;
 
     & > div {
       position: relative;
@@ -48,11 +49,14 @@
       min-height: 300px;
       width: 100%;
       max-width: 1000px;
+      max-height: calc(100% - 20px);
       background-color: $bg-color;
       border-radius: 10px;
       padding: 15px 20px;
       margin: 20px 100px;
       transition: margin 100ms ease;
+      outline: 2px solid $text-color;
+      overflow: auto;
 
       h5 {
         margin-bottom: 15px;
