74de1f30399f21854a5f7ec5505b5030880dd857
IRHM <37304121+IRHM@users.noreply.github.com>
2023-11-14 19:05:23 +0000

    fix left tooltips

--- src/lib/actions/tooltip.ts
+++ src/lib/actions/tooltip.ts
@@ -21,7 +21,7 @@ export default function tooltip(node: HTMLElement, opts: ToolTipOptions) {
       nrect.y += window.scrollY; // Add scrollY to node dom rect so tooltip shows correcting when page is scrolled down
       if (pos === "left") {
         tooltip.style.left = `${nrect.x - trect.width - 10}px`;
-        tooltip.style.top = `${nrect.y + trect.height / 2 - 10}px`;
+        tooltip.style.top = `${nrect.y + trect.height / 2 - 19.5}px`;
       } else if (pos === "top") {
         tooltip.style.left = `${nrect.x - trect.width / 2 + nrect.width / 2}px`;
         tooltip.style.top = `${nrect.y - trect.height - 5}px`;
