2e6bfc99853c8645dd1eb423affa9888a9cf57a5
IRHM <37304121+IRHM@users.noreply.github.com>
2023-11-18 23:16:09 +0000

    move profile stats to reusable components

---
+++ src/lib/stats/Stat.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let name: string;
+  export let value: string | number;
+  export let large = false;
+</script>
+
+<div>
+  <span class={large ? "large" : ""}>{value}</span>
+  <span>{name}</span>
+</div>
+
+<style lang="scss">
+  div {
+    display: flex;
+    flex-flow: column;
+    flex-grow: 1;
+    padding: 20px 15px;
+    background-color: $accent-color;
+    border-radius: 8px;
+
+    > span:first-child {
+      font-weight: bold;
+      font-size: 20px;
+
+      &.large {
+        font-size: 32px;
+      }
+    }
+
+    > span:last-child {
+      margin-top: auto;
+    }
+  }
+</style>
---
+++ src/lib/stats/Stats.svelte
@@ -0,0 +1,16 @@
+<div>
+  <slot />
+</div>
+
+<style lang="scss">
+  div {
+    display: flex;
+    flex-flow: row;
+    gap: 12px;
+    margin-top: 15px;
+
+    @media screen and (max-width: 440px) {
+      flex-wrap: wrap;
+    }
+  }
+</style>
