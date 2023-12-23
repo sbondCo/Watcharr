1dcbe2d633f3276f66aa09ece4c5d71c1474e898
IRHM <37304121+IRHM@users.noreply.github.com>
2023-12-23 16:21:16 +0000

    import vitePreprocess from @sveltejs/vite-plugin-svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
