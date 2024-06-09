b90dc289f699abd1825d1c206cd8ab1fe2fbd64a
IRHM <37304121+IRHM@users.noreply.github.com>
2024-06-09 05:01:43 +0100

    watched_hooks: Fix response watchedSeason status when watchedSeason was just created

--- server/watched_hooks.go
+++ server/watched_hooks.go
@@ -135,8 +135,7 @@ func hookEpisodeStatusChanged(db *gorm.DB, userId uint, watchedId uint, seasonNu
 			hookResponse.Errors = append(hookResponse.Errors, "failed to update season status to finished")
 			return hookResponse
 		} else {
-			if watchedSeason != nil {
-				hookResponse.WatchedSeason = watchedSeason
+			if hookResponse.WatchedSeason != nil {
 				hookResponse.WatchedSeason.Status = newStatus
 			} else {
 				slog.Error("hookEpisodeStatusChanged: watchedSeason was nil HOW DID THIS HAPPEN? Anyways the client won't be able to update its state with the new season status until it is refreshed.")
