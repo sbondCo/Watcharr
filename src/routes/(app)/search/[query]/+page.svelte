b4cc8f85b785b863fa50ac90fb041eb10e5bb3e7
IRHM <37304121+IRHM@users.noreply.github.com>
2024-06-23 18:09:42 +0100

    search: Use new search routes for filters
    Instead of using a local filter of `multi`+`game` results.

--- src/routes/(app)/search/[query]/+page.svelte
+++ src/routes/(app)/search/[query]/+page.svelte
@@ -14,7 +14,10 @@
     ContentSearchTv,
     GameSearch,
     MediaType,
-    PublicUser
+    MoviesSearchResponse,
+    PeopleSearchResponse,
+    PublicUser,
+    ShowsSearchResponse
   } from "@/types";
   import UsersList from "@/lib/UsersList.svelte";
   import { onDestroy, onMount } from "svelte";
@@ -23,7 +26,7 @@
   import { get } from "svelte/store";
   import { notify } from "@/lib/util/notify.js";
   import Icon from "@/lib/Icon.svelte";
-  import { onNavigate } from "$app/navigation";
+  import { afterNavigate } from "$app/navigation";
 
   type GameWithMediaType = GameSearch & { media_type: "game" };
   type CombinedResult =
@@ -35,44 +38,157 @@
 
   export let data;
 
-  let allSearchResults: CombinedResult[];
-  let searchResults: CombinedResult[];
+  let allSearchResults: CombinedResult[] = [];
+  let searchResults: CombinedResult[] = [];
   let activeSearchFilter: SearchFilterTypes | undefined;
+  let curPage = 0;
+  let maxContentPage = 1;
+  let searchRunning = false;
+  let pageErr: any = "";
+  let contentSearchErr: any;
+
+  const infiniteScrollThreshold = 150;
 
   $: searchQ = $searchQuery;
   $: wList = $watchedList;
-  $: (allSearchResults, activeSearchFilter), filterResults();
+  // $: (allSearchResults, activeSearchFilter), filterResults();
+
+  async function searchMulti(query: string, page: number) {
+    try {
+      return await axios.get<ContentSearch>(`/content/search/multi/${query}?page=${page}`);
+    } catch (err) {
+      console.error("Movies/Tv search failed!", err);
+      notify({ text: "Movie/Tv Search Failed!", type: "error" });
+      contentSearchErr = err;
+      return { data: { results: [], page: 1, total_pages: 1 } };
+    }
+  }
+
+  async function searchMovies(query: string, page: number) {
+    try {
+      page = 1;
+      const movies = await axios.get<MoviesSearchResponse>(
+        `/content/search/movie/${query}?page=${page}`
+      );
+      return movies;
+    } catch (err) {
+      console.error("Movies search failed!", err);
+      notify({ text: "Movie Search Failed!", type: "error" });
+      contentSearchErr = err;
+      return { data: { results: [], page: 1, total_pages: 1 } };
+    }
+  }
+
+  async function searchTv(query: string, page: number) {
+    try {
+      page = 1;
+      const shows = await axios.get<ShowsSearchResponse>(
+        `/content/search/tv/${query}?page=${page}`
+      );
+      return shows;
+    } catch (err) {
+      console.error("Tv search failed!", err);
+      notify({ text: "Tv Search Failed!", type: "error" });
+      contentSearchErr = err;
+      return { data: { results: [], page: 1, total_pages: 1 } };
+    }
+  }
+
+  async function searchPeople(query: string, page: number) {
+    try {
+      page = 1;
+      const people = await axios.get<PeopleSearchResponse>(
+        `/content/search/person/${query}?page=${page}`
+      );
+      return people;
+    } catch (err) {
+      console.error("People search failed!", err);
+      notify({ text: "People Search Failed!", type: "error" });
+      contentSearchErr = err;
+      return { data: { results: [], page: 1, total_pages: 1 } };
+    }
+  }
+
+  async function searchGames(query: string, page: number) {
+    try {
+      const f = get(serverFeatures);
+      if (!f.games) {
+        console.debug("game search is not enabled on this server");
+        return { data: [] };
+      }
+      const games = await axios.get<GameSearch[]>(`/game/search/${query}`);
+      return {
+        data: games.data.map((g) => ({
+          ...g,
+          media_type: "game"
+        })) as GameWithMediaType[]
+      };
+    } catch (err) {
+      console.error("Game search failed!", err);
+      notify({ text: "Game Search Failed!", type: "error" });
+      return { data: [] };
+    }
+  }
 
   async function search(query: string) {
-    const f = get(serverFeatures);
-    if (!f.games) {
-      console.log("Search: Only for movies/tv");
-      allSearchResults = (await axios.get<ContentSearch>(`/content/${query}`)).data.results;
-      searchResults = allSearchResults;
+    if (searchRunning) {
+      console.debug("search: already running");
       return;
     }
-    console.log("Search: For movies/tv and games");
-    // To get around promise.all rejecting both promises when one fails,
-    // catch them separately and return empty object so we can still
-    // display the other media types.
-    const r = await Promise.all([
-      axios.get<ContentSearch>(`/content/${query}`).catch((err) => {
-        console.error("Movies/Tv search failed!", err);
-        notify({ text: "Movie/Tv Search Failed!", type: "error" });
-        return { data: { results: [] } };
-      }),
-      axios.get<GameSearch[]>(`/game/search/${query}`).catch((err) => {
-        console.error("Game search failed!", err);
-        notify({ text: "Game Search Failed!", type: "error" });
-        return { data: [] };
-      })
-    ]);
-    const games: GameWithMediaType[] = r[1].data.map((g) => ({
-      ...g,
-      media_type: "game"
-    }));
-    allSearchResults = new Array<CombinedResult>().concat.apply([], [r[0].data.results, games]);
-    searchResults = allSearchResults;
+    if (curPage === maxContentPage) {
+      console.debug("search: max page reached");
+      return;
+    }
+    searchRunning = true;
+    try {
+      if (activeSearchFilter) {
+        // If we have a search filter selected, search for just one specific type of content.
+        console.log("Search: A filter is active:", activeSearchFilter);
+        if (activeSearchFilter === "movie") {
+          allSearchResults = (await searchMovies(query, curPage + 1)).data.results;
+        } else if (activeSearchFilter === "tv") {
+          allSearchResults = (await searchTv(query, curPage + 1)).data.results;
+        } else if (activeSearchFilter === "person") {
+          allSearchResults = (await searchPeople(query, curPage + 1)).data
+            .results as CombinedResult[];
+        } else if (activeSearchFilter === "game") {
+          allSearchResults = (await searchGames(query, curPage + 1)).data;
+        } else {
+          console.error("Active search filter is invalid:", activeSearchFilter);
+        }
+      } else {
+        // If no search filter is applied, do default multi+game combined search.
+        console.log("Search: No filter is applied.");
+        const r = await Promise.all([
+          searchMulti(query, curPage + 1),
+          searchGames(query, curPage + 1)
+        ]);
+        if (r[0].data.total_pages) {
+          maxContentPage = r[0].data.total_pages;
+        }
+        allSearchResults.push(
+          ...new Array<CombinedResult>().concat.apply([], [r[0].data.results, r[1].data])
+        );
+      }
+      console.debug("allSearchResults:", allSearchResults);
+      searchResults = allSearchResults;
+      curPage++;
+    } catch (err) {
+      console.error("search failed!", err);
+      pageErr = err;
+    }
+    searchRunning = false;
+  }
+
+  async function doCleanSearch() {
+    if (!data.slug) {
+      console.error("doCleanSearch: No query to use.");
+      return;
+    }
+    curPage = 0;
+    allSearchResults = [];
+    searchResults = [];
+    search(data.slug);
   }
 
   async function searchUsers(query: string) {
@@ -82,23 +198,45 @@
   function setActiveSearchFilter(to: SearchFilterTypes) {
     if (activeSearchFilter === to) {
       activeSearchFilter = undefined;
-      return;
+    } else {
+      activeSearchFilter = to;
     }
-    activeSearchFilter = to;
+    doCleanSearch();
   }
 
-  function filterResults() {
-    if (!activeSearchFilter) {
-      searchResults = allSearchResults;
-      return;
+  async function infiniteScroll() {
+    if (
+      window.innerHeight + Math.round(window.scrollY) + infiniteScrollThreshold >=
+      document.body.offsetHeight
+    ) {
+      console.log("reached end");
+      window.removeEventListener("scroll", infiniteScroll);
+      if (data.slug) await search(data.slug);
+      window.addEventListener("scroll", infiniteScroll);
     }
-    searchResults = allSearchResults.filter((s) => s.media_type === activeSearchFilter);
   }
 
   onMount(() => {
     if (!searchQ && data.slug) {
       searchQuery.set(data.slug);
     }
+    doCleanSearch();
+
+    // window.addEventListener("scroll", infiniteScroll);
+    // window.addEventListener("resize", infiniteScroll);
+
+    // return () => {
+    //   window.removeEventListener("scroll", infiniteScroll);
+    //   window.removeEventListener("resize", infiniteScroll);
+    // };
+  });
+
+  afterNavigate(() => {
+    console.log("Query changed, performing new search");
+    if (!searchQ && data.slug) {
+      searchQuery.set(data.slug);
+    }
+    doCleanSearch();
   });
 
   onDestroy(() => {
@@ -109,6 +247,8 @@
 <svelte:head>
   <title>Search Results{data?.slug ? ` for '${data?.slug}'` : ""}</title>
 </svelte:head>
+
+<span style="position: sticky;top: 70px;">{curPage} / {maxContentPage}</span>
 
 <div class="content">
   <div class="inner">
@@ -121,84 +261,72 @@
         <PageError pretty="Failed to load users!" error={err} />
       {/await}
 
-      {#await search(data.slug)}
-        <Spinner />
-      {:then}
-        <div class="results-filters-header">
-          <h2>Results</h2>
-          <div>
-            {#if allSearchResults?.length > 0}
-              {#if allSearchResults.find((s) => s.media_type === "movie") || activeSearchFilter === "movie"}
-                <button
-                  class="plain"
-                  data-active={activeSearchFilter === "movie"}
-                  on:click={() => setActiveSearchFilter("movie")}
-                >
-                  <Icon i="film" wh={20} /> Movies
-                </button>
-              {/if}
-              {#if allSearchResults.find((s) => s.media_type === "tv") || activeSearchFilter === "tv"}
-                <button
-                  class="plain"
-                  data-active={activeSearchFilter === "tv"}
-                  on:click={() => setActiveSearchFilter("tv")}
-                >
-                  <Icon i="tv" wh={20} /> TV Shows
-                </button>
-              {/if}
-              {#if allSearchResults.find((s) => s.media_type === "game") || activeSearchFilter === "game"}
-                <button
-                  class="plain"
-                  data-active={activeSearchFilter === "game"}
-                  on:click={() => setActiveSearchFilter("game")}
-                >
-                  <Icon i="gamepad" wh={20} /> Games
-                </button>
-              {/if}
-              {#if allSearchResults.find((s) => s.media_type === "person") || activeSearchFilter === "person"}
-                <button
-                  class="plain"
-                  data-active={activeSearchFilter === "person"}
-                  on:click={() => setActiveSearchFilter("person")}
-                >
-                  <Icon i="people-nocircle" wh={20} /> People
-                </button>
-              {/if}
-            {/if}
-          </div>
+      <div class={`results-filters-header${searchRunning ? " search-running" : ""}`}>
+        <h2>Results</h2>
+        <div>
+          <button
+            class="plain"
+            data-active={activeSearchFilter === "movie"}
+            on:click={() => setActiveSearchFilter("movie")}
+          >
+            <Icon i="film" wh={20} /> Movies
+          </button>
+          <button
+            class="plain"
+            data-active={activeSearchFilter === "tv"}
+            on:click={() => setActiveSearchFilter("tv")}
+          >
+            <Icon i="tv" wh={20} /> TV Shows
+          </button>
+          <button
+            class="plain"
+            data-active={activeSearchFilter === "game"}
+            on:click={() => setActiveSearchFilter("game")}
+          >
+            <Icon i="gamepad" wh={20} /> Games
+          </button>
+          <button
+            class="plain"
+            data-active={activeSearchFilter === "person"}
+            on:click={() => setActiveSearchFilter("person")}
+          >
+            <Icon i="people-nocircle" wh={20} /> People
+          </button>
         </div>
-        <PosterList>
-          {#if searchResults?.length > 0}
-            {#each searchResults as w (w.id)}
-              {#if w.media_type === "person"}
-                <PersonPoster id={w.id} name={w.name} path={w.profile_path} />
-              {:else if w.media_type === "game"}
-                <GamePoster
-                  media={{
-                    id: w.id,
-                    coverId: w.cover.image_id,
-                    name: w.name,
-                    summary: w.summary,
-                    firstReleaseDate: w.first_release_date
-                  }}
-                  {...getPlayedDependedProps(w.id, wList)}
-                  fluidSize
-                />
-              {:else}
-                <Poster
-                  media={w}
-                  {...getWatchedDependedProps(w.id, w.media_type, wList)}
-                  fluidSize
-                />
-              {/if}
-            {/each}
-          {:else}
-            No Search Results!
-          {/if}
-        </PosterList>
-      {:catch err}
-        <Error pretty="Failed to load results!" error={err} />
-      {/await}
+      </div>
+      <PosterList>
+        {#if searchResults?.length > 0}
+          {#each searchResults as w, i (`${i}-${w.media_type}-${w.id}`)}
+            {#if w.media_type === "person"}
+              <PersonPoster id={w.id} name={w.name} path={w.profile_path} />
+            {:else if w.media_type === "game"}
+              <GamePoster
+                media={{
+                  id: w.id,
+                  coverId: w.cover.image_id,
+                  name: w.name,
+                  summary: w.summary,
+                  firstReleaseDate: w.first_release_date
+                }}
+                {...getPlayedDependedProps(w.id, wList)}
+                fluidSize
+              />
+            {:else}
+              <Poster media={w} {...getWatchedDependedProps(w.id, w.media_type, wList)} fluidSize />
+            {/if}
+          {/each}
+        {:else if !searchRunning}
+          No Search Results!
+        {/if}
+      </PosterList>
+
+      {#if searchRunning}
+        <Spinner />
+      {/if}
+
+      {#if contentSearchErr}
+        <Error pretty="Failed to load results!" error={contentSearchErr} />
+      {/if}
     {:else}
       <h2>No Search Query!</h2>
     {/if}
@@ -256,6 +384,13 @@
       @media screen and (max-width: 500px) {
         width: 100%;
         justify-content: center;
+      }
+    }
+
+    &.search-running {
+      button {
+        opacity: 0.8;
+        pointer-events: none;
       }
     }
   }
