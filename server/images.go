9a1b4ef3849a02d12de89246c6e195186ab1be18
IRHM <37304121+IRHM@users.noreply.github.com>
2024-09-19 22:54:40 +0100

    test breaking formatting

--- server/images.go
+++ server/images.go
@@ -25,7 +25,7 @@ import (
 
 // For user uploaded images
 type Image struct {
-	ID        uint      `gorm:"primarykey" json:"-"`
+	ID        uint       `gorm:"primarykey" json:"-"`
 	CreatedAt time.Time `json:"createdAt"`
 	Hash      string    `gorm:"uniqueIndex;not null" json:"-"`
 	BlurHash  string    `json:"blurHash"`
