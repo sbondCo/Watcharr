0d69751fb84a8b894867759813be1d58d033c414
IRHM <37304121+IRHM@users.noreply.github.com>
2025-02-19 19:48:09 +0000

    AboutModal: Add text to gh/site links, use Modal title prop, add attribution file link
    , added a hover effect to all links in the modal and removed the margin on h5.
    
    Also updated the top paragraph.

--- src/lib/nav/AboutModal.svelte
+++ src/lib/nav/AboutModal.svelte
@@ -9,16 +9,13 @@
 	let { onClose }: Props = $props();
 </script>
 
-<Modal title="" {onClose} maxWidth="400px">
+<Modal title="About" {onClose} maxWidth="400px">
 	<div class="content">
 		<div class="inner">
-			<h2>About Watcharr</h2>
-			<h5 class="norm">
-				Watcharr is an open source project developed by <a
-					href="https://github.com/sbondCo/Watcharr/commits?author=IRHM">IRHM</a
-				>
-				and other great people.
-			</h5>
+			<p>
+				An open source project that helps you keep the content you are watching
+				or playing organized and places you in control.
+			</p>
 			<div class="horizontal-icon-list">
 				<a
 					href="https://github.com/sbondCo/Watcharr"
@@ -26,16 +23,20 @@
 					title="Github"
 				>
 					<Icon i="github" wh={60} />
+					Source<br />
+					Code
 				</a>
 				<a
 					href="https://watcharr.app/"
 					target="_blank"
-					title="Watcharr Project Page"
+					title="Watcharr Website & Documentation"
 				>
 					<Icon i="website" wh={60} />
+					Website<br />
+					& Docs
 				</a>
 			</div>
-			<h5 class="norm">Watcharr uses following media databases</h5>
+			<h5 class="norm">Watcharr uses the following media databases:</h5>
 			<div class="horizontal-icon-list">
 				<a href="https://www.themoviedb.org/" target="_blank" title="TMDB">
 					<Icon i="tmdb" wh={60} />
@@ -44,6 +45,18 @@
 					<Icon i="igdb" wh={60} />
 				</a>
 			</div>
+			<h5 class="norm">
+				<!-- Linking to the file in `dev` branch would break every old version of
+				 Watcharr if the file were ever moved in the future. Using the current version
+				 as the branch should ensure this file is always accessible (but only as up to
+				 date as the current version) -->
+				<a
+					href="https://github.com/sbondCo/Watcharr/blob/v{__WATCHARR_VERSION__}/ATTRIBUTION.md"
+					target="_blank"
+				>
+					See our attribution file.
+				</a>
+			</h5>
 		</div>
 	</div>
 </Modal>
@@ -53,13 +66,16 @@
 		margin-bottom: 16px;
 	}
 
-	h5.norm {
-		margin: 8px 25px;
-		text-align: center;
+	p {
+		margin: 8px 0px;
 	}
 
-	h5.norm > a {
-		text-decoration: underline;
+	h5.norm {
+		margin: 8px 0px;
+
+		> a {
+			text-decoration: underline;
+		}
 	}
 
 	div.horizontal-icon-list {
@@ -68,10 +84,22 @@
 		align-items: center;
 		padding-bottom: 20px;
 		padding-top: 5px;
+
+		> a {
+			display: flex;
+			flex-flow: column;
+			align-items: center;
+			gap: 5px;
+			margin: 0 10px;
+			text-align: center;
+		}
 	}
 
-	div.horizontal-icon-list > a {
-		display: inline;
-		margin: 0 10px;
+	a {
+		transition: opacity 150ms ease-in;
+
+		&:hover {
+			opacity: 0.5;
+		}
 	}
 </style>
