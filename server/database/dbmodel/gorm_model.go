b8598ba40412c600751573496f2be13dd4701e3b
IRHM <irhm@sbond.co>
2026-06-27 23:31:48 +0100

    Remove deleted_at columns for watched episode/season tables
    We don't use these columns and won't in the foreseeable future. Query speed seems be to be faster now that we aren't filtering for the extra deleted_at cols.

--- server/database/dbmodel/gorm_model.go
+++ server/database/dbmodel/gorm_model.go
@@ -12,3 +12,11 @@ type GormModel struct {
 	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
 }
+
+// Same as GormModel, but without the DeletedAt field,
+// so use this for tables where we don't need soft deletion.
+type GormModelNoDel struct {
+	ID        uint      `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
--- server/database/entity/watched_season.go
+++ server/database/entity/watched_season.go
@@ -4,7 +4,7 @@ import "github.com/sbondCo/Watcharr/database/dbmodel"
 
 // UniqueIndex applied between WatchedID and SeasonNumber to avoid duplicates incase logic fails.
 type WatchedSeason struct {
-	dbmodel.GormModel
+	dbmodel.GormModelNoDel
 	UserID       uint          `json:"-" gorm:"not null"`
 	User         User          `json:"-"`
 	WatchedID    uint          `json:"-" gorm:"uniqueIndex:ws_watched_to_season_num;not null"`
